291624d08815f32baca6763504f4b98b87ebc560
Osamaessayed <Osamaessayed.el.4468@azhar.edu.eg>
2023-01-07 06:36:35 +0200

    i did the migration of users table

---
+++ migrations/sqls/20230107041955-users-table-up.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    email VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR
+);
